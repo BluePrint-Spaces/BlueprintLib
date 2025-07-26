8270403c61ab9dbc78711b7bf95b131dd860fd56
flippycereal <brodiebabcock1@gmail.com>
2025-07-26 12:59:44 -0400

    Fixed the math ofr ScreenQuadRenderer

--- src/main/java/blueprint/blueprintlib/shaders/ScreenQuadRenderer.java
+++ src/main/java/blueprint/blueprintlib/shaders/ScreenQuadRenderer.java
@@ -18,16 +18,16 @@ public class ScreenQuadRenderer {
 
     private void initQuad() {
         float[] vertices = {
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f,  0.5f, 0f,
-                -0.5f,  0.5f, 0f
+                -1f, -1f,
+                1f, -1f,
+                1f,  1f,
+                -1f,  1f
         };
 
         vao = GL30.glGenVertexArrays();
         vbo = GL15.glGenBuffers();
 
-        GL30.glBindVertexArray(vao);  // Must be bound before glVertexAttribPointer
+        GL30.glBindVertexArray(vao);
 
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
         GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertices, GL15.GL_STATIC_DRAW);
@@ -35,7 +35,6 @@ public class ScreenQuadRenderer {
         GL20.glEnableVertexAttribArray(0);
         GL20.glVertexAttribPointer(0, 2, GL11.GL_FLOAT, false, 2 * Float.BYTES, 0);
 
-        // Only unbind after setting up everything
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
         GL30.glBindVertexArray(0);
 
